af1980fe9436e3597e4edc205adb63195bd7093d
TejasSaraf <saraftejas12@gmail.com>
2024-12-21 21:39:12 -0800

    added more skills

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -8,92 +8,302 @@ function Skills() {
     <div className="skills">
         <section>
             <h1>Skills</h1>
-            <div>
+            <div className="skillsSection">
                 <button className="languages">languages</button>
             <div className="technical">
                 <div className="competencies">
-                    <div>
+                    <div className="logo">
                     <Image className="pythonLogo" src="/python.png" 
                             alt="Python Logo" 
                             width={200} 
                             height={100} />
                     </div>
                     <div>
-                    <h3>Python</h3>
+                    <span>Python</span>
                     </div>
                 </div>
 
                 <div className="competencies">
-                    <div>
+                    <div className="logo">
                     <Image className="javaLogo" src="/java.png" 
                             alt="Java Logo" 
                             width={200} 
                             height={100} />
                     </div>
                     <div>
-                    <h3>Java</h3>
+                    <span>Java</span>
                     </div>
                 </div>
 
                 <div className="competencies">
-                    <div>
+                    <div className="logo">
                     <Image className="javascriptLogo" src="/javascript.png" 
                             alt="javascript Logo" 
                             width={200} 
                             height={100} />
                     </div>
                     <div>
-                    <h3>Javascript</h3>
+                    <span>Javascript</span>
                     </div>
                 </div>
 
                 <div className="competencies">
-                    <div>
+                    <div className="logo">
                     <Image className="typescriptLogo" src="/typescript.png" 
                             alt="Typescript Logo" 
                             width={200} 
                             height={100} />
                     </div>
                     <div>
-                    <h3>Typescript</h3>
+                    <span>Typescript</span>
                     </div>
                 </div>
 
                 <div className="competencies">
-                    <div>
+                    <div className="logo">
                     <Image className="htmlLogo" src="/html.png" 
                             alt="Html Logo" 
                             width={200} 
                             height={100} />
                     </div>
                     <div>
-                    <h3>HTML</h3>
+                    <span>HTML</span>
                     </div>
                 </div>
 
                 <div className="competencies">
-                    <div>
+                    <div className="logo">
                     <Image className="cssLogo" src="/css.png" 
                             alt="CSS Logo" 
                             width={200} 
                             height={100} />
                     </div>
                     <div>
-                    <h3>CSS</h3>
+                    <span>CSS</span>
                     </div>
                 </div>
 
                 <div className="competencies">
-                    <div>
+                    <div className="logo">
                     <Image className="cppLogo" src="/C++.png" 
                             alt="C++ Logo" 
                             width={200} 
                             height={100} />
                     </div>
                     <div>
-                    <h3>C++</h3>
+                    <span>C++</span>
                     </div>
                 </div>
+            </div>
+            </div>
+
+            <div className="skillsSection">
+                <button className="languages">frontend</button>
+            <div className="technical">
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="pythonLogo" src="/Angular.png" 
+                            alt="Python Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>AngularJs</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="javaLogo" src="/react.png" 
+                            alt="Java Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>ReactJs</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="javascriptLogo" src="/next.png" 
+                            alt="javascript Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>NextJs</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="htmlLogo" src="/html.png" 
+                            alt="Html Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>HTML</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="cssLogo" src="/css.png" 
+                            alt="CSS Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>CSS</span>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+
+            <div className="skillsSection">
+                <button className="languages">backend</button>
+            <div className="technical">
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="pythonLogo" src="/Mysql.png" 
+                            alt="Python Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>MYSQL</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="javaLogo" src="/mongodb.png" 
+                            alt="Java Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>MongoDB</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="javascriptLogo" src="/express.png" 
+                            alt="javascript Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>ExpressJs</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="typescriptLogo" src="/node.png" 
+                            alt="Typescript Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>NodeJs</span>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+
+            <div className="skillsSection">
+                <button className="languages">tools</button>
+            <div className="technical">
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="pythonLogo" src="/aws.png" 
+                            alt="Python Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>AWS</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="javaLogo" src="/gcp.png" 
+                            alt="Java Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>GCP</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="javascriptLogo" src="/git.png" 
+                            alt="javascript Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>Git</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="typescriptLogo" src="/github.png" 
+                            alt="Typescript Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>Github</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="typescriptLogo" src="/kubernetes.png" 
+                            alt="Typescript Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>Kubernetes</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="typescriptLogo" src="/docker.png" 
+                            alt="Typescript Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>Docker</span>
+                    </div>
+                </div>
+
+                <div className="competencies">
+                    <div className="logo">
+                    <Image className="typescriptLogo" src="/postman.svg" 
+                            alt="Typescript Logo" 
+                            width={200} 
+                            height={100} />
+                    </div>
+                    <div>
+                    <span>Postman</span>
+                    </div>
+                </div>
+
             </div>
             </div>
         </section>
