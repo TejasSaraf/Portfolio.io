4da6fc2f010597f2a3526444e9680bcdc43ccdce
TejasSaraf <saraftejas12@gmail.com>
2025-01-14 18:20:29 -0600

    Improved footer section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,15 +1,55 @@
 "use client";
+import Image from "next/image";
 
 function Footer() {
   return (
-    <div id="footer">
-      <div className="footerSection">
-          <div className="mt-4 patentContent">
-          <footer className="flex flex-col items-center gap-2 p-6">
-            <span className="font-thin z-10">© Tejas Saraf, 2025</span>
-          </footer>
-          </div>
-        </div>
+    <div className="footerSection">
+      <div className="contact">
+        <a
+          href="mailto:saraftejas12@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image alt="" src="/Gmail.png" width={100} height={100}></Image>
+        </a>
+
+        <a href="https://github.com/TejasSaraf">
+          <Image alt="" src="/Github.svg" width={100} height={100}></Image>
+        </a>
+
+        <a href="https://www.linkedin.com/in/saraf-tejas">
+          <Image alt="" src="/Linkedin.svg" width={100} height={100}></Image>
+        </a>
+
+        <a href="https://x.com/TejasRajendraS1">
+          <Image alt="" src="/Twitter.svg" width={100} height={100}></Image>
+        </a>
+
+        <a href="https://leetcode.com/u/TejasSaraf/">
+          <Image alt="" src="/leetcode.svg" width={100} height={100}></Image>
+        </a>
+      </div>
+
+      <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div>Tejas Saraf</div>
+        <div> • </div>
+        <div>© 2025</div>
+        <div> • </div>
+        <a href="/">Software Developer</a>
+      </div>
+
+      <div className="text-sm text-gray-500 dark:text-gray-400">
+        <p>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/timlrx/tailwind-nextjs-starter-blog"
+          >
+            Built with Tailwind Nextjs Theme
+          </a>
+        </p>
+        <p></p>
+      </div>
     </div>
   );
 }
